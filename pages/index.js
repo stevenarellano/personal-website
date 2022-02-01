c4a96131038855aec679380b85104db496f78ff3
Steven Arellano <stevenjarellano2@gmail.com>
2022-02-01 05:29:32 -0600

    color pallete

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.scss";
+
+export default function Home() {
+    return <div className={styles.container}>the start of something</div>;
+}
