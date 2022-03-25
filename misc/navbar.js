6f5272a98ea622d61baeee28cbe7b42df2d5b791
stevenarellano <stevenjarellano2@gmail.com>
2022-03-24 23:46:30 -0700

    maid

---
+++ misc/navbar.js
@@ -0,0 +1,11 @@
+import { Box, Flex } from "@chakra-ui/react";
+import styles from "../styles/Home.module.css";
+
+export default function Navbar() {
+  return (
+    <Flex p="1rem">
+      <Box>sdf</Box>
+      <Box>sdf</Box>
+    </Flex>
+  );
+}
