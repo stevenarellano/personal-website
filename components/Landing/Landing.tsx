2daa13ff3009d89f1453ec36b09affbf1ebd6c32
steven arellano <stevenjarellano2@gmail.com>
2022-10-10 22:57:55 -0500

    landing page: no animation

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,7 +1,44 @@
 const Landing = () => {
 
     return (
-        <div>page</div>
+        <div className='landing-container'>
+            <div id='black-offset' />
+            <div id='landing-header'>
+                <h1>STEVEN ARELLANO.</h1>
+                <h3>fullstack developer</h3>
+            </div>
+            <div id='social-boxes'>
+                <a
+                    href='https://www.linkedin.com/in/stevenjarellano/'
+                    target='_blank'
+                    className='social-box social-content'>
+                    linkedin
+                </a>
+                <a className='social-box social-hidden' />
+                <a className='social-box social-hidden' />
+                <a className='social-box social-filled' />
+                <a
+                    href='https://github.com/stevenarellano'
+                    target='_blank'
+                    className='social-box social-content'>
+                    github
+                </a>
+                <a className='social-box social-hidden' />
+                <a
+                    href='https://medium.com/@stevenjarellano'
+                    target='_blank'
+                    className='social-box social-content'>
+                    medium
+                </a>
+                <a
+                    href=''
+                    target='_blank'
+                    className='social-box social-content'>
+                    email
+                </a>
+                <a className='social-box social-filled' />
+            </div>
+        </div>
     );
 };
 
