4b32da664655da48ef1237e95153f3739e62924e
steven arellano <stevenjarellano2@gmail.com>
2023-03-16 07:08:49 -0700

    card

--- components/Experiences/Experience.tsx
+++ components/Experiences/Experience.tsx
@@ -10,22 +10,44 @@ type ExperienceProps = {
     bB?: boolean;
 };
 
-const Experience: React.FC<ExperienceProps> = ({ info, bR = false, bB = false }) => {
+const Card1 = ({ info, bR = false, bB = false }: ExperienceProps) => {
     const { company, logo, current } = info;
     const { active, toggleModal } = useToggleModal();
+    const experienceClasses = [styles.card1, bR && styles.bR, bB && styles.bB].join(' ');
 
-    const experienceClasses = [styles.experience, bR && styles.bR, bB && styles.bB].join(' ');
 
     return (
         <div onClick={toggleModal} className={experienceClasses}>
-            <ExperienceModal info={info} active={active} toggleModal={toggleModal} />
             <img alt='logo' src={logo} />
             <div className={styles.info}>
                 <div className={styles.title}>{company}</div>
                 {current && <div className={styles.current}>[current]</div>}
             </div>
+            <ExperienceModal info={info} active={active} toggleModal={toggleModal} />
         </div>
+    );
+};
 
+const Card2 = ({ info, bR = false, bB = false }: ExperienceProps) => {
+    const { company, logo, current } = info;
+    const { active, toggleModal } = useToggleModal();
+    const experienceClasses = [styles.card2, bR && styles.bR, bB && styles.bB].join(' ');
+
+    return (
+        <div onClick={toggleModal} className={experienceClasses}>
+            {current && <div className={styles.current}>[current]</div>}
+            <div className={styles.title}>{company}</div>
+            <img alt='logo' src={logo} />
+            <ExperienceModal info={info} active={active} toggleModal={toggleModal} />
+        </div>
+    );
+};
+
+
+
+const Experience: React.FC<ExperienceProps> = ({ info, bR = false, bB = false }) => {
+    return (
+        <Card1 info={info} bR={bR} bB={bB} />
     );
 };
 
