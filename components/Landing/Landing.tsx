38f845a69e0daf3b0c4f3db649ed4e9009f89d7c
steven arellano <stevenjarellano2@gmail.com>
2023-04-19 16:25:58 -0500

    responsive landing

--- components/Landing/Landing.tsx
+++ components/Landing/Landing.tsx
@@ -1,5 +1,67 @@
 import { useEffect, useState } from 'react';
 import styles from '/styles/modules/landing.module.scss';
+import Image from 'next/image';
+
+const SocialBoxes = () => (
+    <div className={styles.socialBoxes}>
+        <a
+            href='https://www.linkedin.com/in/stevenjarellano/'
+            target='_blank'
+            rel="noreferrer"
+            className={[styles.socialContent, styles.bL, styles.bT, styles.bR].join(' ')}>
+            linkedin
+        </a>
+        <a className={styles.socialHidden} />
+        <a className={styles.socialHidden} />
+        <a className={styles.socialFilled} />
+        <a
+            href='https://github.com/stevenarellano'
+            target='_blank'
+            rel="noreferrer"
+            className={[styles.socialContent, styles.bT, styles.bR].join(' ')}>
+            github
+        </a>
+        <a className={styles.socialHidden} />
+        <a
+            href='https://medium.com/@stevenjarellano'
+            target='_blank'
+            rel="noreferrer"
+            className={[styles.socialContent, styles.bT, styles.bB, styles.bL, styles.bR].join(' ')}>
+            medium
+        </a>
+        <a
+            href='https://twitter.com/_stevenarellano'
+            target='_blank'
+            rel="noreferrer"
+            className={[styles.socialContent, styles.bT, styles.bB, styles.bR].join(' ')}>
+            twitter
+        </a>
+        <a className={styles.socialFilled} />
+    </div>
+);
+
+const LocationBox = () => (
+    <div className={styles.locationBox}>
+        <div className={styles.locationPin}>
+            <Image alt='icon' src='/assets/map-pin.svg' height={20} width={20} />
+        </div>
+        <div className={styles.locationText}>New York City</div>
+        <div className={styles.locationIcon}>
+            <Image alt='location' src='/landing/nyc.png' width={200} height={200} />
+        </div>
+    </div>
+);
+
+const SocialsList = () => (
+    <div className={styles.socialsList}>
+        <div>LINKS</div>
+        <a href='https://www.linkedin.com/in/stevenjarellano/' target='_blank' rel="noreferrer">[LINKEDIN]</a>
+        <a href='https://github.com/stevenarellano' target='_blank' rel="noreferrer">[GITHUB]</a>
+        <a href='https://medium.com/@stevenjarellano' target='_blank' rel="noreferrer">[MEDIUM]</a>
+        <a href='https://twitter.com/_stevenarellano' target='_blank' rel="noreferrer">[TWITTER]</a>
+    </div>
+);
+
 
 const Landing = () => {
     const [active, setActive] = useState(false);
@@ -12,46 +74,14 @@ const Landing = () => {
         <div className={`${styles.landingContainer} page-container ${active ? styles.showActive : ""}`}>
             <div className={styles.blackOffset} />
             <div className={styles.header}>
-                <h1>STEVEN ARELLANO.</h1>
-                <h3>buildr</h3>
+                <div>STEVEN ARELLANO.</div>
+                <div>buildr</div>
             </div>
-            <div className={styles.socialBoxes}>
-                <a
-                    href='https://www.linkedin.com/in/stevenjarellano/'
-                    target='_blank'
-                    rel="noreferrer"
-                    className={[styles.socialContent, styles.bL, styles.bT, styles.bR].join(' ')}>
-                    linkedin
-                </a>
-                <a className={styles.socialHidden} />
-                <a className={styles.socialHidden} />
-                <a className={styles.socialFilled} />
-                <a
-                    href='https://github.com/stevenarellano'
-                    target='_blank'
-                    rel="noreferrer"
-                    className={[styles.socialContent, styles.bT, styles.bR].join(' ')}>
-                    github
-                </a>
-                <a className={styles.socialHidden} />
-                <a
-                    href='https://medium.com/@stevenjarellano'
-                    target='_blank'
-                    rel="noreferrer"
-                    className={[styles.socialContent, styles.bT, styles.bB, styles.bL, styles.bR].join(' ')}>
-                    medium
-                </a>
-                <a
-                    href='https://twitter.com/_stevenarellano'
-                    target='_blank'
-                    rel="noreferrer"
-                    className={[styles.socialContent, styles.bT, styles.bB, styles.bR].join(' ')}>
-                    twitter
-                </a>
-                <a className={styles.socialFilled} />
-            </div>
+            <SocialBoxes />
+            <LocationBox />
+            <SocialsList />
         </div>
     );
 };
 
-export default Landing;
+export default Landing;;
