8951020c1abe43b93244d437bbba09e6d773ab85
Steven Arellano <stevenjarellano2@gmail.com>
2025-03-24 14:32:42 -0500

    Remove some things.

--- src/data/talksAndPapers.ts
+++ src/data/talksAndPapers.ts
@@ -8,13 +8,6 @@ export type TalksAndPapersInfo = {
 
 const TALKS_AND_PAPERS: TalksAndPapersInfo[] = [
 	{
-		title: 'Shape n’ Swarm: Manipulation-Aware Generative Actuation with Swarm UI and LLMs',
-		description:
-			'A research paper on swarm manipulation and generative actuation using Swarm UI and LLMs, currently under review for CHI 2025.',
-		type: 'paper',
-		location: 'CHI 2025',
-	},
-	{
 		title: 'A Primer On Initialization',
 		description:
 			'A lightning talk on initialization techniques in C++ presented at CppCon 2024.',
