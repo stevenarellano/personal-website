06624b90efaa8a7ea5ccf1812ab2c6bc63d3941a
steven arellano <stevenjarellano2@gmail.com>
2025-04-29 19:50:00 -0500

    Remove blog from front page.

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,6 @@ const NAV_DATA = [
   { name: 'talks', link: '#talks' },
   { name: 'awards', link: '#awards' },
   { name: 'projects', link: '#projects' },
-  { name: 'blog', link: '/blog' },
 ];
 
 function App() {
